da241eba64d8b96c7bbb4900d75cb1e65d25125b
atritech <manojatri80@gmail.com>
2022-07-21 15:27:34 +0530

    Updates

---
+++ precache-manifest.45cd7b0cdb407e487ec8e2616cfef34d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e731eaabadec054834d",
+    "url": "/rjmovieapp/static/css/main.e5b5f87b.chunk.css"
+  },
+  {
+    "revision": "3e731eaabadec054834d",
+    "url": "/rjmovieapp/static/js/main.3e731eaa.chunk.js"
+  },
+  {
+    "revision": "5abfe78485241f07799d",
+    "url": "/rjmovieapp/static/js/1.5abfe784.chunk.js"
+  },
+  {
+    "revision": "b4dfc8cf08d4ebafb55d",
+    "url": "/rjmovieapp/static/js/runtime~main.b4dfc8cf.js"
+  },
+  {
+    "revision": "54b1911b15422087236d8041e4d95433",
+    "url": "/rjmovieapp/index.html"
+  }
+];
